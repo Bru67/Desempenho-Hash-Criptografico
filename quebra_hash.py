d73e4798a0149e0d320d57d3d43ee55ee5be028b
Bruna Carnelossi <brunascarnelossi4@gmail.com>
2025-06-04 20:50:32 -0300

    Correção da função carregar_hashes( )

--- quebra_hash.py
+++ quebra_hash.py
@@ -15,8 +15,18 @@ charset = string.digits +string.ascii_letters + string.punctuation
 def carregar_hashes(caminho_arquivo):
     with open(caminho_arquivo, 'r') as f:
         usuarios = json.load(f)
-    return [usuario['senha'] or usuario['senha']['hash'] for usuario in usuarios]
+        caminho_return = []
 
+        for usuario in usuarios:
+            senha = usuario.get('senha')
+            if isinstance(senha, dict): # Para solucao.py --> verifica se 'senha' é um dicionário
+                hash = senha.get('hash')
+                if hash:
+                    caminho_return.append(hash)
+            elif isinstance(senha, str): # Para auth.py --> verifica se 'senha' é uma string
+                caminho_return.append(senha)
+        return caminho_return
+    
 # Força bruta: tenta combinações até encontrar o hash correspondente
 def quebrar_hash(hash_alvo, charset, tamanho_min, tamanho_max):
     for tamanho in range(tamanho_min, tamanho_max + 1):
@@ -29,27 +39,31 @@ def quebrar_hash(hash_alvo, charset, tamanho_min, tamanho_max):
 
 # Função principal com medição de tempo
 def quebrar_senhas_do_arquivo(caminho_arquivo, charset, tamanho_min, tamanho_max):
-    hashes = carregar_hashes(caminho_arquivo)
-    tempos_individuais = []
-    inicio_total = time.time()
+    try:
+        hashes = carregar_hashes(caminho_arquivo)
+        tempos_individuais = []
+        inicio_total = time.time()
 
-    for idx, hash_alvo in enumerate(hashes, 1):
-        print(f"\nQuebrando senha {idx}...")
-        inicio = time.time()
-        senha = quebrar_hash(hash_alvo, charset=charset, tamanho_min=4, tamanho_max=4)
-        fim = time.time()
-        tempo_gasto = fim - inicio
-        tempos_individuais.append(tempo_gasto)
-        if senha:
-            print(f"Senha encontrada com sucesso: {senha} (tempo: {tempo_gasto:.2f} segundos)")
-        else:
-            print(f"Não foi possível encontrar a senha (tempo: {tempo_gasto:.2f} segundos)")
+        for idx, hash_alvo in enumerate(hashes, 1):
+            print(f"\nQuebrando senha {idx}...")
+            inicio = time.time()
+            senha = quebrar_hash(hash_alvo, charset=charset, tamanho_min=4, tamanho_max=4)
+            fim = time.time()
+            tempo_gasto = fim - inicio
+            tempos_individuais.append(tempo_gasto)
+            if senha:
+                print(f"Senha encontrada com sucesso: {senha} (tempo: {tempo_gasto:.2f} segundos)")
+            else:
+                print(f"Não foi possível encontrar a senha (tempo: {tempo_gasto:.2f} segundos)")
 
-    fim_total = time.time()
-    tempo_total = fim_total - inicio_total
-    print(f"\n Tempo total para quebrar todas as senhas: {tempo_total:.2f} segundos")
-    for i, t in enumerate(tempos_individuais, 1):
-        print(f" - Senha {i}: {t:.2f} segundos")
+        fim_total = time.time()
+        tempo_total = fim_total - inicio_total
+        print(f"\n Tempo total para quebrar todas as senhas: {tempo_total:.2f} segundos")
+        for i, t in enumerate(tempos_individuais, 1):
+            print(f" - Senha {i}: {t:.2f} segundos")
+
+    except Exception as e:
+        print(f"\n Ocorreu um erro: {e}.\n")
 
 
 quebrar_senhas_do_arquivo("users.json", charset, tamanho_min=4, tamanho_max=4)
